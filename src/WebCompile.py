3a9ab03eee9de25db24a558a23add2d0e601e304
Lucas Machi <sumolucas@gmail.com>
2023-11-19 08:14:24 -0500

    Webspite formatting changes, new page for resource, gitignore updated

--- src/WebCompile.py
+++ src/WebCompile.py
@@ -1,17 +1,18 @@
 import argparse
 from flask import Flask, render_template_string
 
+app = Flask(__name__)
+
 # Set up the argument parser
 parser = argparse.ArgumentParser(description='Serve Congregational Visualizations.')
 parser.add_argument('base', help='Base HTML to be used to display the site')
 parser.add_argument('ideas', help='HTML file containing the Congregational Ideas visualization.')
 parser.add_argument('strengths', help='HTML file containing the Congregational Strengths visualization.')
 parser.add_argument('fears', help='HTML file containing the Congregational Fears visualization.')
+parser.add_argument('report', help='HTML file containing the report.')
 
 # Parse the arguments
 args = parser.parse_args()
-
-app = Flask(__name__)
 
 # Initialize cache dictionary
 cache = {}
@@ -31,14 +32,17 @@ def index():
 
 @app.route('/<tab_name>')
 def show_tab(tab_name):
-    content = ""
-    if tab_name == 'ideas':
-        content = get_content(args.ideas, 'ideas')
-    elif tab_name == 'strengths':
-        content = get_content(args.strengths, 'strengths')
-    elif tab_name == 'fears':
-        content = get_content(args.fears, 'fears')
-    return render_template_string(open(args.base).read(), content=content, active_tab=tab_name)
+    # Set the active tab and get the corresponding content
+    active_tab = tab_name
+    content = get_content(getattr(args, tab_name), tab_name)
+    return render_template_string(open(args.base).read(), content=content, active_tab=active_tab)
+
+@app.route('/report')
+def report():
+    # Serve the report page and set the active tab to 'report'
+    # Include a new variable to indicate that it's the report page
+    return render_template_string(open(args.base).read(), content=open(args.report).read(), active_tab='report', is_report=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
